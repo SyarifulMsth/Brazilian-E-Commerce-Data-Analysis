88a59d243de178be8ebef8e4908ed8ca520515c1
Syariful Musthofa <60634669+SyarifulMsth@users.noreply.github.com>
2024-04-17 11:28:58 +0700

    Update About_Me.py

--- Dashboard/pages/About_Me.py
+++ Dashboard/pages/About_Me.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
 st.markdown("<h2 style='text-align: center; color: black;'>Creator</h2>", unsafe_allow_html=True)
-st.divider()
 
 st.write('**Name:** Syariful Musthofa')
 st.write('**Education:** Fresh Graduate in Informatics from PGRI Semarang University')
 st.write('**Contact:** [LinkedIn](https://www.linkedin.com/in/syariful-musthofa/) or [GitHub](https://github.com/SyarifulMsth/)')
 st.write('**Thanks for stopping by!**')
 
-st.divider()
