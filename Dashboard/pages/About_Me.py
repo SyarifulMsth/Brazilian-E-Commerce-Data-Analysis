6d25c7daa2c432153c25a84be9c4d2911624bde7
Syariful Musthofa <60634669+SyarifulMsth@users.noreply.github.com>
2024-04-17 11:28:30 +0700

    Update About_Me.py

--- Dashboard/pages/About_Me.py
+++ Dashboard/pages/About_Me.py
@@ -2,11 +2,10 @@ import streamlit as st
 
 st.markdown("<h2 style='text-align: center; color: black;'>Creator</h2>", unsafe_allow_html=True)
 st.divider()
-with st.container():
-    col1 = st.columns(1)[0]
 
-    col1.write('**Name:** Syariful Musthofa')
-    col1.write('**Education:** Fresh Graduate in Informatics from PGRI Semarang University')
-    col1.write('**Contact:** [LinkedIn](https://www.linkedin.com/in/syariful-musthofa//) or [GitHub](https://github.com/SyarifulMsth/)')
-    col1.write('**Thanks for stopping by!**')
+st.write('**Name:** Syariful Musthofa')
+st.write('**Education:** Fresh Graduate in Informatics from PGRI Semarang University')
+st.write('**Contact:** [LinkedIn](https://www.linkedin.com/in/syariful-musthofa/) or [GitHub](https://github.com/SyarifulMsth/)')
+st.write('**Thanks for stopping by!**')
+
 st.divider()
